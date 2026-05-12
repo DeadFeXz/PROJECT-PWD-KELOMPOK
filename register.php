9c4725e2d645132001636324478326d7a068a635
DeadFeXz <se4ur7@gmail.com>
2026-05-12 13:44:39 +0700

    Penambahan logo upnvy di icon tab

--- register.php
+++ register.php
@@ -96,6 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
+<link rel="icon" type="image/png" href="assets/upnvylogo.png">
 
 </head>
 
